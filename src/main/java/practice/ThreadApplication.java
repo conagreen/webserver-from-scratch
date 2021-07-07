e7eac53c64acebfedce23140181d2fd8569f3f58
conagreen <icoqwee@gmail.com>
2021-07-07 11:25:11 +0900

    쓰레드 연습

--- src/main/java/practice/ThreadApplication.java
+++ src/main/java/practice/ThreadApplication.java
@@ -6,11 +6,14 @@ public class ThreadApplication {
         final Thread thread2 = new Thread(new Task());
         // 우선 순위 설정
         thread1.setPriority(1);
-//        thread1.setDaemon();
+
+        // setDaemon은 기본값이 false
+        thread1.setDaemon(true);
+        thread2.setPriority(10);
         thread1.start();
-        thread1.setPriority(10);
+//        thread2.setDaemon(true);
         thread2.start();
-        Thread.sleep(3000);
+        Thread.sleep(1);
         System.out.println("메인 - 일어남" + Thread.currentThread().getName());
 
     }
@@ -25,7 +28,7 @@ class Task implements Runnable {
         for (int i = 1; i < 100_0000; i++) {
             l *= i;
         }
-        System.out.println("나고 난 후 : " + Thread.currentThread().getName());
+        System.out.println("자고 난 후 : " + Thread.currentThread().getName());
 
     }
 }
