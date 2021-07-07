1efe18a5a4dba06b361e1fb0b05b7b5fe018365b
conagreen <icoqwee@gmail.com>
2021-07-07 18:56:43 +0900

    요청 - getHeader(), httpMethod(), requestUri() 구현

--- src/main/java/com/github/conagreen/HttpRequest.java
+++ src/main/java/com/github/conagreen/HttpRequest.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * HTTP Request
@@ -13,32 +15,45 @@ import java.io.InputStreamReader;
  * */
 public class HttpRequest {
 
+    private String requestLine;
+    private String method;
+    private String uri;
+    private Map<String, String> headerMap = new HashMap<>();
+
     // 생성자
     public HttpRequest(InputStream in) {
         try {
             final BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            // 요청 라인 파싱
+            this.requestLine = br.readLine();
+            final String[] tokens = this.requestLine.split(" ");
+            this.method = tokens[0];
+            this.uri =  tokens[1];
+
+            // 헤더 파싱
             String line;
             while (!(line = br.readLine()).equals("")) {
-                System.out.println(line);
+//                System.out.println(line);
+                final String[] keyAndValue = line.split(":", 2);
+                headerMap.put(keyAndValue[0].trim(), keyAndValue[1].trim());
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public String getHeader (String headerName) {
-
-        return null;
+        return this.headerMap.get(headerName);
     }
 
     public HttpMethod httpMethod() {
-
-        return null;
+        return HttpMethod.valueOf(method);
     }
 
     public String requestUri() {
-
-        return null;
+        return uri;
     }
 
     public String getQueryString() {
--- src/main/java/com/github/conagreen/RequestHandler.java
+++ src/main/java/com/github/conagreen/RequestHandler.java
@@ -19,6 +19,11 @@ public class RequestHandler implements Runnable {
         try (final InputStream in = socket.getInputStream(); final OutputStream out = socket.getOutputStream()) {
             //요청
             final HttpRequest request = new HttpRequest(in);
+            System.out.println("request.httpMethod() = " + request.httpMethod());
+            System.out.println("request.requestUri() = " + request.requestUri());
+            System.out.println("request.getHeader(\"Host\") = " + request.getHeader("Host"));
+            System.out.println("request.getHeader(\"Accept\") = " + request.getHeader("Accept"));
+            System.out.println("request.getHeader(\"User-Agent\") = " + request.getHeader("User-Agent"));
 
             // 응답
             final HttpResponse response = new HttpResponse(out);
@@ -30,17 +35,6 @@ public class RequestHandler implements Runnable {
                     "<BODY><h1>제목이시다</h1><p>내용이시다</p></BODY>" +
                     "</HTML>";
             final int contentLength = html.getBytes().length;
-
-            /*
-            < 응답 문법 >
-
-            A server sends response messages to the client,
-
-            - a status line, consisting of the protocol version, a space, the response status code, another space, a possibly empty reason phrase, a carriage return, and a line feed (e.g. HTTP/1.1 200 OK);
-            - zero or more response header fields, each consisting of the case-insensitive field name, a colon, optional leading whitespace, the field value, and optional trailing whitespace (e.g. Content-Type: text/html), and ending with a carriage return and a line feed;
-            - an empty line, consisting of a carriage return and a line feed;
-            - an optional message body.
-            */
 
             // 1. 상태 라인 (status line)
             dos.writeBytes("HTTP/1.1 200 OK\r\n");
