9b59837828b661888922bcf5190e09ea3418f4b6
conagreen <icoqwee@gmail.com>
2021-07-12 23:06:26 +0900

    RequestDispatcher 구현 - 컨트롤러 디스패칭

--- src/main/java/com/github/conagreen/webserver/RequestDispatcher.java
+++ src/main/java/com/github/conagreen/webserver/RequestDispatcher.java
@@ -1,2 +1,50 @@
-package com.github.conagreen.webserver;public class RequestDispatcher {
+package com.github.conagreen.webserver;
+
+import com.github.conagreen.controller.Controller;
+import com.github.conagreen.controller.HelloController;
+import com.github.conagreen.http.request.HttpMethod;
+import com.github.conagreen.http.request.HttpRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class RequestDispatcher {
+
+    private static final Controller notFoundController = new NotFoundController();
+    private static final Map<RequestKey, Controller> DISPATCHER_MAP = new HashMap<>();
+
+    static {
+        DISPATCHER_MAP.put(new RequestKey("/hello", HttpMethod.GET), new HelloController());
+    }
+
+    public Controller dispatch(HttpRequest request) {
+        final Controller controller = DISPATCHER_MAP.get(new RequestKey(request.requestUri(), request.httpMethod()));
+        if (controller != null) return controller;
+
+        return notFoundController;
+    }
+}
+
+class RequestKey {
+    private final String uri;
+    private final HttpMethod method;
+
+    public RequestKey(String uri, HttpMethod method) {
+        this.uri = uri;
+        this.method = method;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RequestKey that = (RequestKey) o;
+        return Objects.equals(uri, that.uri) && method == that.method;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uri, method);
+    }
 }
--- src/main/java/com/github/conagreen/webserver/RequestHandler.java
+++ src/main/java/com/github/conagreen/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package com.github.conagreen.webserver;
 
+import com.github.conagreen.controller.Controller;
 import com.github.conagreen.http.request.HttpRequest;
 import com.github.conagreen.http.response.HttpResponse;
 
@@ -9,6 +10,7 @@ import java.net.Socket;
 public class RequestHandler implements Runnable {
 
     private final Socket socket;
+    private static final RequestDispatcher DISPATCHER = new RequestDispatcher();
 
     public RequestHandler(Socket socket) {
         this.socket = socket;
@@ -17,33 +19,13 @@ public class RequestHandler implements Runnable {
     @Override
     public void run() {
         try (final InputStream in = socket.getInputStream(); final OutputStream out = socket.getOutputStream()) {
-            //요청
+            // 요청
             final HttpRequest request = new HttpRequest(in);
-//            System.out.println("request.httpMethod() = " + request.httpMethod());
-//            System.out.println("request.requestUri() = " + request.requestUri());
-//            System.out.println("request.getHeader(\"Host\") = " + request.getHeader("Host"));
-//            System.out.println("request.getHeader(\"Accept\") = " + request.getHeader("Accept"));
-//            System.out.println("request.getHeader(\"User-Agent\") = " + request.getHeader("User-Agent"));
-//            System.out.println("request.getQueryString() = " + request.getQueryString());
-//            System.out.println("request.getParameter(\"name\") = " + request.getParameter("name"));
-
             // 응답
             final HttpResponse response = new HttpResponse(out);
 
-            // 이미지 그리기
-//            final File file = new File("crushed-duck.jpg");
-//            response.writeImage(file);
-
-//            final String html = "" +
-//                    "<HTML>" +
-//                    "<HEAD><TITLE>핸드메이트 웹서버</TITLE></HEAD>" +
-//                    "<BODY><h1>제목이시다</h1><p>내용이시다</p></BODY>" +
-//                    "</HTML>";
-//
-//            response.setStatus(HttpStatus.OK);
-//            response.addHeader("Content-Type", "text/html;charset=utf-8");
-//            response.writeBody(html.getBytes(StandardCharsets.UTF_8));
-            response.sendRedirect("https://www.google.com/");
+            final Controller controller = DISPATCHER.dispatch(request);
+            controller.process(request, response);
         } catch (IOException e) {
             e.printStackTrace();
         }
