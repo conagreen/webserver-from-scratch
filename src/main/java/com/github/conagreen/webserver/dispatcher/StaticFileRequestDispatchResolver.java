4c0955474c28cc8883b504c9fc40e8422699f8af
conagreen <icoqwee@gmail.com>
2021-07-18 13:47:30 +0900

    스태틱 파일 Resolver  추가

--- src/main/java/com/github/conagreen/webserver/dispatcher/StaticFileRequestDispatchResolver.java
+++ src/main/java/com/github/conagreen/webserver/dispatcher/StaticFileRequestDispatchResolver.java
@@ -1,2 +1,37 @@
-package com.github.conagreen.webserver.dispatcher;public class StaticFileRequestDispatchResolver {
+package com.github.conagreen.webserver.dispatcher;
+
+import com.github.conagreen.controller.Controller;
+import com.github.conagreen.http.request.HttpRequest;
+import com.github.conagreen.webserver.StaticFileController;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StaticFileRequestDispatchResolver implements RequestDispatchResolver {
+
+    private static final String STATIC_FILE_DIR = "static";
+    private static final Controller STATIC_FILE_CONTROLLER = new StaticFileController();
+
+    @Override
+    public boolean supports(HttpRequest request) {
+        final String filepath = STATIC_FILE_DIR + request.requestUri();
+        InputStream in = null;
+
+        try {
+            in = this.getClass().getClassLoader().getResourceAsStream(filepath);
+            return in != null;
+        }finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    @Override
+    public Controller dispatch(HttpRequest request) {
+        return STATIC_FILE_CONTROLLER;
+    }
 }
