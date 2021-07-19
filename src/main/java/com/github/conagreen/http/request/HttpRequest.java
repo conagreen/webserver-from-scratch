9936b2707405510add81d37d8c3e3af94c2df5b3
conagreen <icoqwee@gmail.com>
2021-07-19 20:27:43 +0900

    요청바디(POST로 요청 시) 추가 구현

--- src/main/java/com/github/conagreen/http/request/HttpRequest.java
+++ src/main/java/com/github/conagreen/http/request/HttpRequest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,14 +18,31 @@ public class HttpRequest {
 
     private final RequestLine requestLine;
     private final RequestHeaders headers;
+    private final Map<String, String> parameterMap = new HashMap<>();
 
     // 생성자
     public HttpRequest(InputStream in) {
         try {
             final BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            // 요청라인
             this.requestLine = new RequestLine(br.readLine());
             this.headers = new RequestHeaders(br);
+
+            final String contentType = headers.getHeader("Content-Type");
+            final String lengthHeaderValue = headers.getHeader("Content-Length");
+            if (lengthHeaderValue != null) {
+                if ("application/x-www-form-urlencoded".equals(contentType)) {
+                    final int contentLength = Integer.parseInt(lengthHeaderValue);
+                    char[] body = new char[contentLength];
+                    br.read(body);
+                    final String rawFormData = String.copyValueOf(body);
+                    Arrays.stream(rawFormData.split("&"))
+                            .forEach(kv -> {
+                                final String[] keyAndValue = kv.split("=");
+                                parameterMap.put(keyAndValue[0].trim(), keyAndValue[1].trim());
+                            });
+                }
+            }
+
 
         } catch (IOException e) {
             throw new IllegalStateException("IO 문제 발생");
@@ -49,6 +67,10 @@ public class HttpRequest {
     }
 
     public String getParameter(String key) {
-        return requestLine.getParameter(key);
+        final String param = requestLine.getParameter(key);
+        if (param != null) {
+            return param;
+        }
+        return parameterMap.get(key);
     }
 }
