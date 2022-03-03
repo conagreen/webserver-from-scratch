e4a03e5022aa33d29ce78ba48220c5135570dcd8
conagreen <icoqwee@gmail.com>
2022-03-03 22:17:43 +0900

    ThreadPoolExecutor 생성 -  스레드 관리

--- src/main/java/com/github/conagreen/webserver/WebServer.java
+++ src/main/java/com/github/conagreen/webserver/WebServer.java
@@ -3,25 +3,24 @@ package com.github.conagreen.webserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 public class WebServer implements Runnable {
+
     @Override
     public void run() {
-        // 포트 8080번에 대기하는 서버소켓 생성
+        final BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>(5);
+        final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 5, 10,
+                TimeUnit.SECONDS, blockingQueue);
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             Socket connection;
-
-            /*
-            accept()가 호출되면 프로그램은 여기서 실행을 멈추고 클라이언트 포트가 8080번으로 연결할 때까지 무한 대기.
-            클라이언트가 연결되면 accept() 메소드는 Socket 객체를 반환한다.
-            반환된 연결은 java.net.Socket 객체 형태로 반환되며 클라이언트에서 사용한 것과 같다.
-            */
             while ((connection = serverSocket.accept()) != null) {
-                final Thread handlerThread = new Thread(new RequestHandler(connection));
-                handlerThread.start();
+                threadPoolExecutor.execute(new RequestHandler(connection));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            threadPoolExecutor.shutdown();
         }
     }
 }
